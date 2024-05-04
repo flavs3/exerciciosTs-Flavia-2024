f598b472198b4b7fb832327ab77fc6bc93941344
flavs3 <flaviag.sacchi@gmail.com>
2024-05-04 13:40:32 -0300

    Ajuste de Commits Final - com header e menu

---
+++ ts/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MenuLateralComponent } from './menu-lateral/menu-lateral.component'; // Verifique o caminho correto
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'DevChuva';
+}
